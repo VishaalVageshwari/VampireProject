ceb49debfb71f3a3959772f217b0db89f851951b
OEM <z5162987@student.unsw.edu.au>
2019-10-31 23:35:34 +1100

    Main templates and routing

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,3 +5,19 @@ from app.main import bp
 @bp.route('/index', methods=['GET'])
 def index():
   return render_template('index.html', title='Home')
+
+@bp.route('/requestBlood', methods=['GET'])
+def requestBloodPage():
+  return render_template('requestBloodPage.html', title='Medical Staff')
+
+@bp.route('/vampireStaff', methods=['GET'])
+def vampireStaffPage():
+  return render_template('vampireStaffPage.html', title='Vampire Staff')
+
+@bp.route('/bloodBank', methods=['GET'])
+def bloodBankPage():
+  return render_template('bloodBankPage.html', title='Blood Bank')
+
+@bp.route('/depositeBlood', methods=['GET'])
+def depositBloodPage():
+  return render_template('depositBloodPage.html', title='Deposit Blood')
