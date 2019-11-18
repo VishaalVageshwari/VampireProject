245a81b41659160ee6c7c7ed64c060f86164658b
Vishaal Vageshwari <vishaal778@gmail.com>
2019-11-18 14:27:57 +1100

    small error not allowing for AB-

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import ValidationError, InputRequired, Email
 from app.models import Blood
 
 BLOOD_TYPES = [('A+', 'A+'), ('A-', 'A-'), ('B+', 'B+'), ('B-', 'B-'), 
-    ('AB+', 'AB+'), ('AB+', 'AB+'), ('O+', 'O+'), ('O-', 'O-')]
+    ('AB+', 'AB+'), ('AB-', 'AB-'), ('O+', 'O+'), ('O-', 'O-')]
 
 SORT_TYPES = [('Default', 'Default'),('Volume: Low-High', 'Volume: Low-High'),
     ('Volume: High-Low', 'Volume: High-Low'), ('Use-By-Date: Earliest-Latest', 'Use-By-Date: Earliest-Latest'),
